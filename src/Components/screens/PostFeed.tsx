b11a408a2d2f0fa9042e067da845d4f48dc62189
Chinmaymuduli <chinmaymuduli0@gmail.com>
2024-05-03 22:38:05 +0530

    feed api integration

--- src/Components/screens/PostFeed.tsx
+++ src/Components/screens/PostFeed.tsx
@@ -77,8 +77,160 @@ const PostFeed = () => {
           <InputField type="text" placeholder="Search for results" />
         </Input>
       </Box>
-      {postInfo?.map((data: any, index: any) => {
-        const [like, setLike] = useState(data.isLiked);
+      {data?.data?.data?.map((item: any, index: any) => (
+        <View
+          key={index}
+          style={{
+            paddingBottom: 10,
+            borderBottomColor: 'gray',
+            borderBottomWidth: 0.1,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              padding: 15,
+            }}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Image
+                source={
+                  item?.user_id?.avatar
+                    ? {uri: item?.user_id?.avatar}
+                    : IMAGES.USER
+                }
+                style={{width: 40, height: 40, borderRadius: 100}}
+              />
+              <View style={{paddingLeft: 5}}>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    fontFamily: 'Montserrat-Bold',
+                    color: 'black',
+                  }}>
+                  {item?.user_id?.name}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    fontFamily: 'Montserrat-Medium',
+                    color: 'black',
+                  }}>
+                  {`${item?.user_id?.gender}, ${'13'}`}
+                </Text>
+              </View>
+            </View>
+            <Pressable onPress={() => navigate('UserProfile')}>
+              <Feather name="more-vertical" style={{fontSize: 20}} />
+            </Pressable>
+          </View>
+          {item?.media?.length > 0 && (
+            <View
+              style={{
+                position: 'relative',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Image
+                source={{uri: item?.media[0]}}
+                style={{width: '100%', height: 400}}
+              />
+            </View>
+          )}
+
+          <HStack
+            style={{
+              alignItems: 'center',
+              paddingVertical: 15,
+              paddingHorizontal: 12,
+            }}
+            gap={'$2'}>
+            <TouchableOpacity
+            // onPress={() => setLike(!like)}
+            >
+              <AntDesign
+                // name={like ? 'heart' : 'hearto'}
+                name={'heart'}
+                style={{
+                  paddingRight: 10,
+                  fontSize: 20,
+                  color: 'black',
+                  // color: like ? 'red' : 'black',
+                }}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Ionic
+                name="chatbubble-outline"
+                style={{fontSize: 20, paddingRight: 10}}
+                color={'black'}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Feather name="navigation" style={{fontSize: 20}} />
+            </TouchableOpacity>
+          </HStack>
+
+          <View style={{paddingHorizontal: 15}}>
+            {/* <Text style={{fontFamily: 'Montserrat-Medium', fontSize: 13}}>
+              Liked by {like ? 'you and' : ''}{' '}
+              {like ? data.likes + 1 : data.likes} others
+            </Text> */}
+            <Text
+              style={{
+                fontSize: 13,
+                paddingVertical: 2,
+                fontFamily: 'Montserrat-Medium',
+              }}>
+              {item?.caption}
+            </Text>
+            {item?.tags?.length > 0 && (
+              <HStack>
+                {item?.tags?.map((tag, index) => (
+                  <Box key={index}>
+                    <Text>#{tag}</Text>
+                  </Box>
+                ))}
+              </HStack>
+            )}
+            <Text style={{opacity: 0.4, paddingVertical: 2}}>
+              View all comments
+            </Text>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Image
+                  source={IMAGES.USER}
+                  style={{
+                    width: 25,
+                    height: 25,
+                    borderRadius: 100,
+                    backgroundColor: 'orange',
+                    marginRight: 10,
+                  }}
+                />
+                <TextInput
+                  placeholder="Add a comment "
+                  style={{opacity: 0.5}}
+                />
+              </View>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Entypo
+                  name="emoji-happy"
+                  style={{fontSize: 15, color: 'lightgreen', marginRight: 10}}
+                />
+                <Entypo
+                  name="emoji-neutral"
+                  style={{fontSize: 15, color: 'pink', marginRight: 10}}
+                />
+                <Entypo name="emoji-sad" style={{fontSize: 15, color: 'red'}} />
+              </View>
+            </View>
+          </View>
+        </View>
+      ))}
+      {/* {data?.data?.data?.map((data: any, index: any) => {
+        const [like, setLike] = useState(false);
         return (
           <View
             key={index}
@@ -106,7 +258,7 @@ const PostFeed = () => {
                       fontFamily: 'Montserrat-Bold',
                       color: 'black',
                     }}>
-                    {data?.postTitle}
+                    {data?.user_id?.name}
                   </Text>
                   <Text
                     style={{
@@ -222,7 +374,7 @@ const PostFeed = () => {
             </View>
           </View>
         );
-      })}
+      })} */}
     </View>
   );
 };
